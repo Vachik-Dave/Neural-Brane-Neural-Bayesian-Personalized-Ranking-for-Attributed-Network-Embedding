e1e637de759e754339ed8e0a21aefdc056774eff
Vachik-Dave <vachik.dave25@gmail.com>
2018-02-06 16:05:20 -0500

    Neural-Brane

---
+++ Neural-Brane_embedding.py
@@ -0,0 +1,249 @@
+"""
+
+Neural-Brane: End to end implmentation:
+inputs: Network file, attribute file [emb_dimensions, hidden_neurons]
+output: Embedding file
+
+
+Author: Vachik Dave.
+"""
+
+
+import sys
+import numpy as np
+import random
+import my_NueralBPR_gpu
+import datetime
+import argparse
+
+
+def separate_adj_wei(adj_lists):
+	'''
+	For each node separate adjacency list and weights also generate corresponding weight based probability
+	'''
+	new_adj = {};
+	for key in adj_lists:
+		s = adj_lists[key];
+		total = 0.0;
+		neighbors = [];
+		weights = [];
+		for each in s:
+			neighbors.append(each[0]);
+			weights.append(each[1]);
+			total += each[1];
+
+		probs = [ each/total for each in weights];
+		new_adj[key] = [neighbors,probs];
+
+	return new_adj;
+
+def load_data(filename):
+	'''
+	Read network file
+	'''
+	N = 0;
+	M = 0;
+	adj_lists = {}
+	f = open(filename);
+	index = 0;
+	for line in f:
+		l = line.strip().split(" ")
+		if index == 0:
+			N = int(l[0]);
+			M = int(l[1]);
+			index += 1;
+			continue;
+		u = int(l[0]);
+		v = int(l[1]);
+		w = 1.0;
+		try:
+			w = float(l[2]);
+		except IndexError:
+			pass;
+		try:
+			t = adj_lists[u];
+			t.add((v,w));
+			adj_lists[u] = t;
+		except KeyError:
+			t = set([]);
+			t.add((v,w));
+			adj_lists[u] = t;
+
+		try:
+			t = adj_lists[v];
+			t.add((u,w));
+			adj_lists[v] = t;
+		except KeyError:
+			t = set([]);
+			t.add((u,w));
+			adj_lists[v] = t;
+
+	adj_lists = separate_adj_wei(adj_lists);
+
+	return N,M,adj_lists;
+
+def get_degree_dist(adj_lists):
+	'''
+	gnerate degree based probability for all nodes
+	'''
+	node_ids = [];
+	prob = [];
+	total = 0.0;
+	for key in adj_lists:
+		node_ids.append(key);
+		d = float(len(adj_lists[key][0]));
+		val = pow(d,0.75);
+		prob.append(val);
+		total += val;
+
+	for i in range(len(prob)):
+		prob[i] = prob[i]/total;
+
+	return node_ids,prob;
+
+
+def load_feat_data(filename):
+	'''
+	Read node attribute file
+	'''
+	node_feat = {}
+	f = open(filename);
+	index = 0;
+	max_feat_id = 0;
+	prev_u = -1;
+	for line in f:
+		l = line.strip().split(" ")
+		u = 0;
+		try:
+			u = int(l[0]);
+		except ValueError:
+			print "prev index = "+str(index);
+			print "prev node_id = "+str(prev_u);
+			print line;
+			sys.exit();
+		feat_list = [];
+		for i in range(1,len(l)):
+			f = map(int,l[i].strip().split(":"));
+  			feat_list.append(f[0]);
+			if max_feat_id < f[0]:
+				max_feat_id = f[0];
+
+		node_feat[u] = feat_list;
+#		if u == 5408:
+#			print str(u) + " " +str(feat_list);
+		prev_u = u;
+		index += 1;
+	print "max_feat_id: "+str(max_feat_id);
+	return max_feat_id, node_feat;
+
+def save_embedding(filename,embedding):
+	f = open(filename,'w');
+	N,dim = np.array(embedding).shape;
+	f.write(str(N)+" "+str(dim));
+	index = 0;
+	for row in embedding:
+		f.write("\n"+str(index));
+		index += 1;
+		for ele in row:
+			f.write(" "+str(ele));
+	f.close();
+
+def convert_nodeAtt(node_feat,emb,N):
+	'''
+	Get attribute based node embedding for each node from Attribute embedding matrix (P)
+	Similar to embedding layer task
+	'''
+	index = 0;
+	# assuming in nodeAtt file node_ids are in order
+	new_emb = [];
+	emb_dim = len(emb[0]);
+	for i in range(N):
+		feat_list = [];
+		try:
+			feat_list = node_feat[i];
+		except KeyError:
+			a = 0;
+
+		att_emb_row = np.zeros(emb_dim);
+		for i in range(len(feat_list)):
+			idx = feat_list[i];
+			for j in range(len(emb[idx])):
+				if att_emb_row[j] < emb[idx][j]:
+					att_emb_row[j] = emb[idx][j];
+		new_emb.append(att_emb_row);
+		#if len(line) > 1:
+		#	new_emb.append( np.divide(att_emb_row,float(len(line)-1) ) );
+		#else:
+		#	new_emb.append(att_emb_row);
+		index += 1;
+
+	return new_emb;
+
+def parse_args():
+	'''
+	Parses the input arguments.
+	'''
+	parser = argparse.ArgumentParser(description="Run Neural-Brane.")
+	parser.add_argument('-input_graph', nargs='?', help='Input network path')
+	parser.add_argument('-node_att', nargs='?', help='Node attribute file path')
+	parser.add_argument('-dimensions', type=int, default=150, help='Number of dimensions. (default = 150).')
+	parser.add_argument('-num_neurons', type=int, default=150, help='Number of neurons in hidden layer. (default = 150).')
+	parser.add_argument('-num_epoch', type=int, default=50, help='Number of echops. (default = 50).')
+	parser.add_argument('-learning_rate', type=float, default=0.05, help='Learning rate (default = 0.05).')
+	parser.add_argument('-regularation_rate', type=float, default=0.0001, help='Regularation rate (default = 0.0001).')
+def main():
+
+	start_t = datetime.datetime.now()
+	network_filename = sys.argv[1];
+	feat_filename = sys.argv[2];
+
+	N,M,adj_lists = load_data(network_filename);
+	print "# of nodes: "+str(N);
+	node_ids,prob = get_degree_dist(adj_lists);
+
+	max_feat_id, node_feat = load_feat_data(feat_filename);
+
+	hidden_dim = int(sys.argv[3]);
+	num_neurons = int(sys.argv[4]);
+
+#	num_epoch = 25;
+	num_epoch = 30;
+#	num_epoch = int(sys.argv[5]);
+#	num_iter = 5000;
+#	batch_size = 100;
+	num_iter = 1000;
+	batch_size = 100;
+	learning_rate = 0.5;
+#	learning_rate = float(sys.argv[6]);
+	regulation_rate = 0.0001;
+
+	print "learning rate:" +str(learning_rate);
+	print "num_epoch:" + str(num_epoch);
+
+	
+	start_t = datetime.datetime.now();
+
+	# only used for linked prediction to avoid training on test instances
+	test_pos = {};
+	node_emb, att_emb = my_NueralBPR_gpu.main(hidden_dim,num_epoch,num_iter,batch_size,N,adj_lists,test_pos, learning_rate, regulation_rate, num_neurons, max_feat_id, node_feat,node_ids,prob)
+
+	print "node_emb shape: " + str(np.array(node_emb).shape)
+
+	print "att_emb shape: " + str(np.array(att_emb).shape);
+
+	node_att_emb = convert_nodeAtt(node_feat,att_emb,N);
+
+	print "node_att_emb shape: " + str(np.array(node_att_emb).shape);
+
+	node_emb_combined = np.hstack((node_emb[:-1],node_att_emb));
+	
+	print "node_emb_combined shape: " + str(np.array(node_emb_combined).shape);
+
+	end_t = datetime.datetime.now();
+
+	print "Embedding timing: "+str(end_t - start_t);
+
+	emb_filename1 = network_filename + "_combined_Emb_" +str(learning_rate)+"_"+str(hidden_dim)+"_"+str(num_neurons) +".txt";
+	save_embedding(emb_filename1,node_emb_combined);
+
+main();
---
+++ my_NueralBPR_gpu.py
@@ -0,0 +1,271 @@
+"""
+
+Neural-Brane: Tensorflow GPU implementation.
+
+Author: Vachik Dave
+
+"""
+
+
+import numpy as np
+import tensorflow as tf
+import os
+import random
+
+def generate_train_batch(adj_lists, test_pos, N,node_ids,prob, batch_size=512):
+	'''
+	uniform sampling (node, neighbor node, non-neighbor node)
+	'''
+	t = []
+	# select list of non-neighbors based on degree
+	j_list = np.random.choice(node_ids, batch_size,p=prob); #randint(1, N-1)
+	for b in xrange(batch_size):
+		# random first node
+		u = random.sample(adj_lists.keys(), 1)[0]
+		# weight based sample neighbor node
+		i = np.random.choice(adj_lists[u][0],p = adj_lists[u][1]);
+		ind = 0;
+		try:
+			while i == test_pos[u]:
+				#i = random.sample(adj_lists[u], 1)[0]
+				i = np.random.choice(adj_lists[u][0],p = adj_lists[u][1]);
+				ind += 1;
+				if ind == 100:
+					u = random.sample(adj_lists.keys(), 1)[0]
+					i = np.random.choice(adj_lists[u][0],p = adj_lists[u][1]);
+					ind = 0;
+		except KeyError:
+			pass;
+
+		j = j_list[len(t)];
+		ind = 0;
+		# if the j is in neighbor list
+		while j in adj_lists[u][0]:
+			j = np.random.choice(node_ids,p=prob); #randint(1, N-1)
+			#j = random.randint(1, N-1);
+			ind += 1;
+			if ind == 100:
+				print "Error: Training batch generation, cannot find negative sample for u = ", u;
+				break;
+
+		t.append([u, i, j])
+	return np.asarray(t)
+
+
+def uij_to_feat(uij,node_feat,max_feat_id):
+	'''
+	Generate same length attribiute set for each triple with max-attribute_ID+2 as padding
+	'''
+	uij_feat = [];
+	max_feat_count = 0;
+	for each in uij:
+		u = each[0];
+		i = each[1];
+		j = each[2];
+		u_feat_list = [];
+		i_feat_list = [];
+		j_feat_list = [];
+		try:
+			u_feat_list = node_feat[u];
+		except KeyError:
+			print "Error: features not found... u = ", u ;
+
+		try:
+			i_feat_list = node_feat[i];
+		except KeyError:
+			print "Error: features not found... i = ", i ;
+
+		try:
+			j_feat_list = node_feat[j];
+		except KeyError:
+			print "Error: features not found... j = ", j ;
+
+
+		if max_feat_count < len(u_feat_list):
+			max_feat_count = len(u_feat_list);
+		if max_feat_count < len(i_feat_list):
+			max_feat_count = len(i_feat_list);
+		if max_feat_count < len(j_feat_list):
+			max_feat_count = len(j_feat_list);
+
+		uij_feat.append([u_feat_list,i_feat_list,j_feat_list]);
+
+	uij_feat_list = [];
+	#add padding of max_feat_id+2
+	for each in uij_feat:
+		new_u_list = each[0] + [max_feat_id+2]*(max_feat_count-len(each[0]));
+		new_i_list = each[1] + [max_feat_id+2]*(max_feat_count-len(each[1]));
+		new_j_list = each[2] + [max_feat_id+2]*(max_feat_count-len(each[2]));
+
+		uij_feat_list.append([new_u_list,new_i_list,new_j_list]);
+
+	return np.asarray(uij_feat_list)		
+
+def uij_to_adj(uij,adj_lists,test_pos,N):
+	'''
+	Generate same length neighborhood set for each triple with N+2 as padding
+	'''
+	uij_adj = [];
+	max_nei_count = 0;
+	for each in uij:
+		u_adj = [];
+		i_adj = [];
+		j_adj = [];
+		try:
+			u_adj = adj_lists[each[0]][0];
+		except KeyError:
+			print "Error: adj list not found... u = ", each ;
+
+		try:
+			i_adj = adj_lists[each[1]][0];
+		except KeyError:
+			print "Error: adj list not found... i = ", each ;
+
+		try:
+			j_adj = adj_lists[each[2]][0];
+		except KeyError:
+			print "Error: adj list not found... j = ", each ;
+
+
+
+		if max_nei_count < len(u_adj):
+			max_nei_count = len(u_adj);
+		if max_nei_count < len(i_adj):
+			max_nei_count = len(i_adj);
+		if max_nei_count < len(j_adj):
+			max_nei_count = len(j_adj);
+
+		uij_adj.append([u_adj,i_adj,j_adj]);
+
+	uij_adj_list = [];
+	#add padding of N+2
+	for each in uij_adj:
+		new_u_list = each[0] + [N+2]*(max_nei_count-len(each[0]));
+		new_i_list = each[1] + [N+2]*(max_nei_count-len(each[1]));
+		new_j_list = each[2] + [N+2]*(max_nei_count-len(each[2]));
+
+		uij_adj_list.append([new_u_list,new_i_list,new_j_list]);
+
+	return np.asarray(uij_adj_list)		
+
+
+def neural_bpr(N, max_feat_id, node_feat, hidden_dim,num_neurons,learning_rate = 0.01, regulation_rate = 0.0001):
+	'''
+	Neural-Brane architecture: intputs -> Embedding layer (lookup+max-pooling+integration) -> hidden layer -> output layer ->BPR layer
+	'''
+	#inputs: For triplets (u,i,j)
+	#neighborhood information (adjacency vectors)
+	u = tf.placeholder(tf.int32, [None,None])
+	i = tf.placeholder(tf.int32, [None,None])
+	j = tf.placeholder(tf.int32, [None,None])
+	#attribute information
+	u_att = tf.placeholder(tf.int32, [None,None])
+	i_att = tf.placeholder(tf.int32, [None,None])
+	j_att = tf.placeholder(tf.int32, [None,None])
+
+	'''
+	------------------------------------------- Embedding Layer -------------------------------------------------------------
+	'''
+	with tf.device("/gpu:0"):
+		#----------------------------- attribute embedding --------------------------------------------------------------
+
+		node_att_emb_w = tf.get_variable("node_feat_emb_w", [max_feat_id+1, hidden_dim], initializer=tf.random_normal_initializer(0, 0.01))
+		# Can add bias for each attribtes
+		#node_att_b = tf.get_variable("node_att_b", [max_feat_id+1, 1],initializer=tf.constant_initializer(0.0))
+
+		# CONCAT-LOOKUP (.)
+		u_emb1 = tf.reduce_max(tf.nn.embedding_lookup(node_att_emb_w,u_att),1)
+		i_emb1 = tf.reduce_max(tf.nn.embedding_lookup(node_att_emb_w,i_att),1)
+		j_emb1 = tf.reduce_max(tf.nn.embedding_lookup(node_att_emb_w,j_att),1)
+
+		#i_b1 = tf.reduce_max(tf.nn.embedding_lookup(node_att_b, i_att),1)
+		#j_b1 = tf.reduce_max(tf.nn.embedding_lookup(node_att_b, j_att),1)
+
+
+		#----------------------------- topological embedding --------------------------------------------------------------
+
+		node_emb_w = tf.get_variable("node_emb_w", [N+1, hidden_dim], initializer=tf.random_normal_initializer(0, 0.01))
+
+		# Can add bias for each attribtes
+		#node_b = tf.get_variable("node_b", [N+1, 1],initializer=tf.constant_initializer(0.0))
+
+		# CONCAT-LOOKUP (.)
+		u_emb2 = tf.reduce_max(tf.nn.embedding_lookup(node_emb_w,u),1)
+		i_emb2 = tf.reduce_max(tf.nn.embedding_lookup(node_emb_w,i),1)
+		j_emb2 = tf.reduce_max(tf.nn.embedding_lookup(node_emb_w,j),1)
+
+		#i_b2 = tf.reduce_max(tf.nn.embedding_lookup(node_b, i),1)
+		#j_b2 = tf.reduce_max(tf.nn.embedding_lookup(node_b, j),1)
+
+
+		#----------------------------- Integration component --------------------------------------------------------------
+
+		u_emb3 = tf.concat([u_emb1,u_emb2],1);
+		i_emb3 = tf.concat([i_emb1,i_emb2],1);
+		j_emb3 = tf.concat([j_emb1,j_emb2],1);
+#		i_b = tf.concat([i_b1,i_b2],1);
+#		j_b = tf.concat([j_b1,j_b2],1);
+		
+		'''
+		----------------------------- NN Hidden Layer --------------------------------------------------------------
+		'''		
+		NN_w1 = tf.get_variable("nn_w1", [hidden_dim*2,num_neurons], initializer=tf.random_normal_initializer(0, 0.1))
+		NN_b1 = tf.get_variable("nn_b1", [num_neurons],initializer=tf.constant_initializer(0.0))
+
+		u_emb = tf.nn.relu(tf.matmul(u_emb3, NN_w1));
+		i_emb = tf.nn.relu(tf.matmul(i_emb3, NN_w1));
+		j_emb = tf.nn.relu(tf.matmul(j_emb3, NN_w1));
+#		u_emb = tf.nn.relu(tf.matmul(u_emb3, NN_w1) + NN_b1);
+#		i_emb = tf.nn.relu(tf.matmul(i_emb3, NN_w1) + NN_b1);
+#		j_emb = tf.nn.relu(tf.matmul(j_emb3, NN_w1) + NN_b1);
+
+
+	'''
+	------------------------------------------- Output + BPR Layer -------------------------------------------------------------
+	'''
+	x = tf.reduce_sum(tf.multiply(u_emb, (i_emb - j_emb)), 1, keep_dims=True)
+	l2_norm = tf.add_n([tf.reduce_sum(tf.multiply(u_emb, u_emb)), tf.reduce_sum(tf.multiply(i_emb, i_emb)),tf.reduce_sum(tf.multiply(j_emb, j_emb))]);
+
+	bprloss = regulation_rate * l2_norm - tf.reduce_mean(tf.log(tf.sigmoid(x)))
+
+
+	'''
+				Back-propogation learning using gradient descent 
+	'''
+	train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(bprloss)
+	
+	return u, i, j, u_att, i_att, j_att, bprloss, train_op, node_emb_w, node_att_emb_w;
+
+
+def main(hidden_dim,num_epoch,num_iteration,batch_size,N,adj_lists,test_pos, learning_rate, regulation_rate, num_neurons, max_feat_id, node_feat,node_ids,prob):
+	'''
+	tensorflow session
+	'''
+	with tf.Graph().as_default(), tf.Session() as session:
+
+		u, i, j, u_att, i_att, j_att, loss, train_op, node_emb_w, node_att_emb_w = neural_bpr(N, max_feat_id, node_feat, hidden_dim,num_neurons,learning_rate, regulation_rate)
+		print 'construct tensorflow computational graph done'
+		session.run(tf.global_variables_initializer())
+
+		for epoch in range(num_epoch):
+			_batch_bprloss = 0
+			for k in range(num_iteration): # uniform samples from training set
+				uij = generate_train_batch(adj_lists, test_pos, N,node_ids,prob, batch_size);
+				#print k;
+				uij_feat_list = uij_to_feat(uij,node_feat,max_feat_id);
+				uij_adjList = uij_to_adj(uij,adj_lists,test_pos,N);
+
+#				ui_label = get_paired_input(uij);
+#				ui_feat_list, labels = ui_to_feat(ui_label,node_feat,max_feat_id);
+#				print "training batch generated...";
+
+				_bprloss, _ = session.run([loss, train_op], feed_dict={u:uij_adjList[:,0], i:uij_adjList[:,1], j:uij_adjList[:,2], u_att:uij_feat_list[:,0], i_att:uij_feat_list[:,1], j_att:uij_feat_list[:,2]})
+
+				_batch_bprloss += _bprloss
+
+#			print "epoch: ", epoch
+#			print "bpr_loss: ", _batch_bprloss / k
+			print str(epoch) + "," + str(_batch_bprloss / k);
+
+		node_emb, node_att_emb = session.run([node_emb_w,node_att_emb_w])
+		return node_emb, node_att_emb;
